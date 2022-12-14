148ccc2dea75ba72ffb4ad8cb0910de59325f663
Diederick Niehorster <dcnieho@gmail.com>
2022-12-14 23:05:25 +0100

    test_server: made async version

--- test_server.py
+++ test_server.py
@@ -1,9 +1,99 @@
 import socket
-import selectors
 import traceback
 import threading
 import struct
 import time
+
+import sys
+import asyncio
+import socket
+import struct
+import threading
+import traceback
+
+async def read_with_length(reader):
+    # protocol: first the size of a message is sent so 
+    # receiver knows what to expect. Then the message itself
+    # is sent
+    try:
+        try:
+            msg_size = await reader.readexactly(SIZE_MESSAGE_SIZE)
+        except asyncio.IncompleteReadError:
+            # connection broken
+            return ''
+        msg_size = struct.unpack(SIZE_MESSAGE_FMT, msg_size)[0]
+
+        buf = ''
+        left_over = msg_size
+        while left_over>0:
+            received = (await reader.read(min(4096, left_over))).decode('utf8')
+            if not received:    # connection broken
+                return ''
+            buf += received
+            left_over = msg_size-len(buf)
+        return buf
+
+    except ConnectionError:
+        return 'ConnectionError'
+
+async def send_with_length(writer, message) -> bool:
+    try:
+        to_send = message.encode('utf8')
+
+        # first notify end point of message length
+        writer.write(struct.pack(SIZE_MESSAGE_FMT, len(to_send)))
+
+        # then send message
+        writer.write(to_send)
+
+        await writer.drain()
+        return True
+    except ConnectionError:
+        return False
+
+client_list = []
+async def handle_client(reader, writer):
+    sock = writer.get_extra_info('socket')
+    set_keepalive_windows(sock)
+
+    client_addr = writer.get_extra_info('peername')
+    print('received connection from {}:{}'.format(*client_addr))
+    
+    message = None
+    while message != 'quit':
+        try:
+            message = await read_with_length(reader)
+            if message:
+                print(f'<{client_addr[0]}:{client_addr[1]}> {message}')
+
+                response = message+message
+                if not await send_with_length(writer, response):
+                    # connection broken, close
+                    break
+            else:
+                # connection broken, close
+                break
+ 
+        except Exception as exc:
+            tb_lines = traceback.format_exception(exc)
+            print("".join(tb_lines))
+            continue
+
+    writer.close()
+
+async_addr = None
+async def run_server(server_address):
+    global async_addr
+
+    server = await asyncio.start_server(handle_client, *server_address)
+    async_addr = [sock.getsockname() for sock in server.sockets]
+    if len(async_addr[0])!=2:
+        async_addr[0],async_addr[1] = async_addr[1],async_addr[0]
+    print('serving on {}:{}'.format(*async_addr[0]))
+
+    async with server:
+        await server.serve_forever()    # docs say cancelling this coroutine causes server to stop
+
 
 
 
@@ -50,6 +140,18 @@ def set_keepalive_windows(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
     else:
         sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, int(after_idle_sec*1000), int(interval_sec*1000)))
 
+def set_keepalive(after_idle_sec=1, interval_sec=3, max_fails=5):
+    if sys.platform.startswith("win"):
+        set_keepalive_windows(after_idle_sec, interval_sec, max_fails)
+    elif sys.platform.startswith("linux"):
+        set_keepalive_linux(after_idle_sec, interval_sec, max_fails)
+    elif sys.platform.startswith("darwin"):
+        set_keepalive_osx(after_idle_sec, interval_sec, max_fails)
+    else:
+        print("Your system is not officially supported at the moment!\n"
+              "You can let me know on GitHub, or you can try porting yourself ;)")
+        sys.exit(1)
+
 SIZE_MESSAGE_FMT  = '!i'
 SIZE_MESSAGE_SIZE = struct.calcsize(SIZE_MESSAGE_FMT)
 def send_message_with_length(sock: socket.socket, message) -> bool:
@@ -89,141 +191,7 @@ def receive_message_with_length(sock: socket.socket) -> str:
         return str(buf,'utf-8')
     except ConnectionError:
         return ''
-
-class _ClientList(list):
-    """
-    List of all clients.
-    """
-    def append(self, client):
-        self.reap()
-        super().append(client)
-
-    def pop_all(self):
-        self[:], result = [], self[:]
-        return result
-
-    def join(self):
-        for client in self.pop_all():
-            client.thread.join()
-
-    def reap(self):
-        self[:] = (client for client in self if client.thread.is_alive())
-
-
-class ThreadedTCPServer:
-    def __init__(self, server_address):
-        self.server_address = server_address
-        self.__is_shut_down = threading.Event()
-        self.__shutdown_request = False
-
-        # TODO: this needs to be protected by a lock for operations on the clients
-        self._clients = _ClientList()
-
-        # start server
-        self.socket: socket.socket = \
-            socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(self.server_address)
-        self.server_address = self.socket.getsockname()
-        self.socket.listen(20)
-
-    def fileno(self):
-        """Return socket file number.
-        Interface required by selector.
-        """
-        return self.socket.fileno()
-        
-    def serve_forever(self, poll_interval=0.5):
-        """Handle one request and poll for shutdown
-        every poll_interval seconds.
-        """
-        self.__is_shut_down.clear()
-        try:
-            # XXX: Consider using another file descriptor or connecting to the
-            # socket to wake this up instead of polling. Polling reduces our
-            # responsiveness to a shutdown request and wastes cpu at all other
-            # times.
-            with selectors.DefaultSelector() as selector:
-                selector.register(self, selectors.EVENT_READ)
-
-                while not self.__shutdown_request:
-                    ready = selector.select(poll_interval)
-                    if self.__shutdown_request:
-                        # shutdown() called during select(), exit immediately.
-                        break
-                    if ready:
-                        self._handle_connection()
-
-        finally:
-            self.__shutdown_request = False
-            self.__is_shut_down.set()
-
-    def shutdown(self):
-        """Stops the serve_forever loop.
-        Blocks until the loop has finished. This must be called while
-        serve_forever() is running in another thread, or it will
-        deadlock.
-        """
-        self.__shutdown_request = True
-        self.__is_shut_down.wait()
-
-    def close(self):
-        self.socket.shutdown(socket.SHUT_WR)
-        self.socket.close()
-
-    def _handle_connection(self):
-        try:
-            conn, client_address = self.socket.accept()
-        except OSError:
-            return
-
-        set_keepalive_windows(conn)
-
-        # start thread to serve the client
-        t = ClientThread(conn, client_address)
-        t.start()
-        # add to client list (this also culls any dead clients from list)
-        self._clients.append(t)
-
-        
-class ClientThread:
-    def __init__(self, conn, client_address) -> None:
-        self.conn = conn
-        self.client_address = client_address
-        self.thread = None
-
-    def start(self):
-        self.thread = threading.Thread(target=self.client_thread, daemon=True)
-        self.thread.start()
-
-    def client_thread(self):
-        while True:
-            try:
-                message = self.receive()
-                if message:
-                    print (f'<{self.client_address[0]}:{self.client_address[1]}> {message}')
-
-                    if not self.send('received'):
-                        # connection broken, close
-                        break
-                else:
-                    # connection broken, close
-                    break
- 
-            except Exception as exc:
-                tb_lines = traceback.format_exception(exc)
-                print("".join(tb_lines))
-                continue
-
-        # we're done, shut down
-        self.conn.shutdown(socket.SHUT_WR)
-        self.conn.close()
-
-    def receive(self) -> str:
-        return receive_message_with_length(self.conn)
-
-    def send(self, message):
-        return send_message_with_length(self.conn, message)
-
+    
 
 def client(ip, port, message):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -231,21 +199,47 @@ def client(ip, port, message):
         send_message_with_length(sock, message)
         print("Received: {}".format(receive_message_with_length(sock)))
 
+
+def await_clients(loop, futures):
+    loop.run_until_complete(asyncio.gather(*futures))
+
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", 0
+    def run_loop(loop: asyncio.AbstractEventLoop):
+        asyncio.set_event_loop(loop)
+        try:
+            loop.run_forever()
+        finally:
+            loop.run_until_complete(loop.shutdown_asyncgens())
+            loop.close()
+            
+    loop = asyncio.new_event_loop()
+    thread = threading.Thread(target=run_loop, args=(loop,), daemon=True)
+    thread.start()
+        
+    asyncio.run_coroutine_threadsafe(run_server(("localhost", 0)), loop)
 
-    server = ThreadedTCPServer((HOST, PORT))
-    ip, port = server.server_address
-
-    # Start server thread. Server thread will start one thread to handle each client
-    server_thread = threading.Thread(target=server.serve_forever, daemon=True)
-    server_thread.start()
+    while async_addr is None:
+        time.sleep(.01)
+    ip,port = async_addr[0]
     
-    threading.Thread(target = client, args=[ip, port, "Hello World 1, more text"], daemon=True).start()
-    threading.Thread(target = client, args=[ip, port, "Hello World 2"], daemon=True).start()
-    threading.Thread(target = client, args=[ip, port, "Hello World 3"], daemon=True).start()
+    if True:
+        aas = []
+        aas.append(loop.run_in_executor(None, client, ip, port, "Hello World 1, more text"))
+        aas.append(loop.run_in_executor(None, client, ip, port, "Hello World 2"))
+        aas.append(loop.run_in_executor(None, client, ip, port, "Hello World 3"))
+        
+        done,pending = asyncio.run_coroutine_threadsafe(
+                asyncio.wait(aas, return_when=asyncio.ALL_COMPLETED)
+            , loop).result()
 
-    time.sleep(2)
+    else:
+        ts = []
+        ts.append(threading.Thread(target = client, args=[ip, port, "Hello World 1, more text"], daemon=True))
+        ts.append(threading.Thread(target = client, args=[ip, port, "Hello World 2"], daemon=True))
+        ts.append(threading.Thread(target = client, args=[ip, port, "Hello World 3"], daemon=True))
+        for t in ts:
+            t.start()
 
-    server.shutdown()
+        for t in ts:
+            t.join()
