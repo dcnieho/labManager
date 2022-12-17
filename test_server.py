a1e2bfa47914c45000469b391e19e680d0f3c3a6
Diederick Niehorster <dcnieho@gmail.com>
2022-12-17 18:06:38 +0100

    async setup now in right place got a rid of server address global, just returned by server starter clients now split into two, a client starter and a main loop (not that thats important as client will be a separate simple process, but helps my understanding of async)

--- test_server.py
+++ test_server.py
@@ -1,14 +1,9 @@
-import socket
-import traceback
-import threading
-import struct
-import time
-
 import sys
 import asyncio
 import socket
 import struct
 import threading
+import concurrent
 import traceback
 
 # to allow clients to discover server:
@@ -69,7 +64,6 @@ def set_keepalive(after_idle_sec=1, interval_sec=3, max_fails=5):
 SIZE_MESSAGE_FMT  = '!i'
 SIZE_MESSAGE_SIZE = struct.calcsize(SIZE_MESSAGE_FMT)
 client_list = []
-server_addr = None
 
 async def read_with_length(reader) -> str:
     # protocol: first the size of a message is sent so 
@@ -149,8 +143,6 @@ async def broadcast(message):
         await send_with_length(writer, message)
 
 async def run_server(server_address):
-    global server_addr
-
     server = await asyncio.start_server(handle_client, *server_address)
 
     server_addr = [sock.getsockname() for sock in server.sockets]
@@ -158,24 +150,30 @@ async def run_server(server_address):
         server_addr[0],server_addr[1] = server_addr[1],server_addr[0]
     print('serving on {}:{}'.format(*server_addr[0]))
 
-    async with server:
-        await server.serve_forever()    # docs say cancelling this coroutine causes server to stop
+    asyncio.run_coroutine_threadsafe(server.serve_forever(), loop)
+
+    return server_addr
 
 
-async def client_async(loop, ip, port, id, message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        await loop.sock_connect(sock, (ip, port))
-        reader, writer = await asyncio.open_connection(sock=sock)
+async def client_loop(id, reader, writer):
+    response = None
+    while response != 'quit':
+        print("{} Received: {}".format(id, (response := await read_with_length(reader))))
 
-        await send_with_length(writer, message)
+    writer.close()
 
-        response = None
-        while response != 'quit':
-            print("{} Received: {}".format(id, (response := await read_with_length(reader))))
+async def start_client(loop, ip, port, id, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    await loop.sock_connect(sock, (ip, port))
+    reader, writer = await asyncio.open_connection(sock=sock)
 
-        writer.close()
+    await send_with_length(writer, message)
 
-if __name__ == "__main__":
+    return asyncio.run_coroutine_threadsafe(client_loop(id, reader, writer), loop)
+
+loop = None
+def setup_async():
+    global loop
     def run_loop(loop: asyncio.AbstractEventLoop):
         asyncio.set_event_loop(loop)
         try:
@@ -187,20 +185,21 @@ if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     thread = threading.Thread(target=run_loop, args=(loop,), daemon=True)
     thread.start()
-        
-    asyncio.run_coroutine_threadsafe(run_server(("localhost", 0)), loop)
 
-    while server_addr is None:
-        time.sleep(.01)
-    ip,port = server_addr[0]
+async def main():
+    # start server
+    server_addr = asyncio.run_coroutine_threadsafe(run_server(("localhost", 0)), loop)
+    ip,port = server_addr.result()[0]
     
-    aas = []
-    aas.append(asyncio.run_coroutine_threadsafe(client_async(loop, ip, port, 1, "Hello World 1, more text"),loop))
-    aas.append(asyncio.run_coroutine_threadsafe(client_async(loop, ip, port, 2, "Hello World 2"),loop))
-    aas.append(asyncio.run_coroutine_threadsafe(client_async(loop, ip, port, 3, "Hello World 3"),loop))
+    # start clients
+    aas = [
+        asyncio.run_coroutine_threadsafe(start_client(loop, ip, port, 1, "Hello World 1, more text"),loop),
+        asyncio.run_coroutine_threadsafe(start_client(loop, ip, port, 2, "Hello World 2"),loop),
+        asyncio.run_coroutine_threadsafe(start_client(loop, ip, port, 3, "Hello World 3"),loop)
+    ]
 
-    # give them some time to spin up
-    time.sleep(.1)
+    # wait till clients have started, get futures to their processing loop tasks
+    aas = [f.result() for f in concurrent.futures.as_completed(aas)]
 
     # send some messages to clients
     asyncio.run_coroutine_threadsafe(send_with_length(client_list[1][1],'sup'),loop)
@@ -210,8 +209,14 @@ if __name__ == "__main__":
     for a in aas:
         a.result()
 
-    # give client handler some time to detect endpoint has closed connection
-    time.sleep(.01)
-
+    # give server some time to detect clients have closed connection
+    await asyncio.sleep(.01)
+    
     # see what clients we have left, should be empty if above wait was long enough
     print(client_list)
+
+if __name__ == "__main__":
+    setup_async()
+    asyncio.run(main())
+        
+    
