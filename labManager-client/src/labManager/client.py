abe79ad3654fccfe4209fa9556c819b88c2b4883
Diederick Niehorster <dcnieho@gmail.com>
2023-12-21 19:49:33 +0100

    refactored client: - now separate task list for each master (else task ids would conflict!) - cleaned up typing - now gathered all info about a connected master in a class - now proper locking around master list

--- labManager-client/src/labManager/client.py
+++ labManager-client/src/labManager/client.py
@@ -3,7 +3,8 @@ import traceback
 import platform
 import pathlib
 import json
-from typing import List, Tuple
+import threading
+from dataclasses import dataclass, field
 
 from labManager.common import config, eye_tracker, message, share, task
 from labManager.common.network import comms, ifs, keepalive, ssdp
@@ -26,31 +27,36 @@ async def run(duration: float = None):
     await client.stop()
 
 
+@dataclass
+class ConnectedMaster:
+    writer:         asyncio.streams.StreamWriter
+    remote_addr:    tuple[str,int]
+    local_addr:     tuple[str,int]
+    task_list:      list[task.RunningTask]  = field(default_factory=lambda: [])
+    handler_task:   asyncio.Task = None
+
 class Client:
     def __init__(self, network):
         self.network  = network
         self.name     = platform.node()
 
-        self._ssdp_discovery_task: asyncio.Task = None
-        self._ssdp_client: network.ssdp.Client = None
-        self._if_ips  = None
-        self._if_macs = None
+        self._ssdp_discovery_task:          asyncio.Task                = None
+        self._ssdp_client:                  network.ssdp.Client         = None
+        self._if_ips:                       list[str]                   = None
+        self._if_macs:                      list[str]                   = None
 
-        self._poll_for_eyetrackers_task: asyncio.Task = None
-        self._connected_eye_tracker: eye_tracker.ET_class = None
-        self._handler_tasks    : List[asyncio.Task] = []
-        self._connected_masters: List[Tuple[str,int]] = []
-        self._local_addrs      : List[Tuple[str,int]] = []
-        self._writers          : List[asyncio.streams.StreamWriter] = []
+        self._poll_for_eyetrackers_task:    asyncio.Task                = None
+        self.connected_eye_tracker:         eye_tracker.ET_class        = None
+        self._next_master_id:               int                         = 0
+        self.masters:                       dict[int,ConnectedMaster]   = {}
+        self.master_lock                                                = threading.Lock()
 
-        self._task_list        : List[task.RunningTask] = []
-
-        self._mounted_drives: List[str] = []
+        self._mounted_drives:               list[str]                   = []
 
     def __del__(self):
         self._stop_sync()
 
-    async def start(self, server_addr: Tuple[str,int] = None, *, keep_ssdp_running = False):
+    async def start(self, server_addr: tuple[str,int] = None, *, keep_ssdp_running = False):
         # 1. get interfaces we can work with
         self._if_ips, self._if_macs = ifs.get_ifaces(self.network)
         if not self._if_ips:
@@ -89,79 +95,86 @@ class Client:
         port = int(port) # convert to integer
         await self._start_new_master((ip,port))
 
-    async def _start_new_master(self, server_addr: Tuple[str,int]):
+    async def _start_new_master(self, server_addr: tuple[str,int]):
         # check if we're already connected to this master
         # if so, skip
-        for i,m in enumerate(self._connected_masters):
-            if server_addr==m:
-                return
+        with self.master_lock:
+            for m in self.masters:
+                if server_addr==self.masters[m].remote_addr:
+                    return
 
         # connect to master at specified server_address
         reader, writer = await asyncio.open_connection(
             *server_addr, local_addr=(self._if_ips[0],0))
         keepalive.set(writer.get_extra_info('socket'))
-        self._connected_masters.append(server_addr)
-        self._local_addrs.append(writer.get_extra_info('sockname'))
-        self._writers.append(writer)
+        m = self._get_next_master_id()
+        with self.master_lock:
+            self.masters[m] = \
+                ConnectedMaster(writer, server_addr, writer.get_extra_info('sockname'))
 
         # run connection handler
-        self._handler_tasks.append(asyncio.create_task(self._handle_master(reader, writer)))
+        self.masters[m].handler_task = asyncio.create_task(self._handle_master(m, reader, writer))
+
+    def _get_next_master_id(self):
+        m = self._next_master_id
+        self._next_master_id += 1
+        return m
 
     async def stop(self, timeout=2):
         # stop and cancel everything
         self._stop_sync()
         await asyncio.sleep(0)  # give cancellation a chance to be sent and processed
-        writer_close_waiters = [asyncio.create_task(w.wait_closed()) for w in self._writers]
         self._poll_for_eyetrackers_task.cancel()
 
         # wait till everything is stopped and cancelled
-        running_tasks = [x.async_task for x in self._task_list]
+        running_tasks = [t.async_task for m in self.masters for t in self.masters[m].task_list]
         await asyncio.wait(
             running_tasks +
             self._poll_for_eyetrackers_task +
             ([asyncio.create_task(self._ssdp_client.stop())] if self._ssdp_client else []) +
-            writer_close_waiters +
-            self._handler_tasks,
+            [asyncio.create_task(self.masters[m].writer.wait_closed()) for m in self.masters] +
+            [self.masters[m].handler_task for m in self.masters],
             timeout=timeout
         )
 
         # clear out state
         self._poll_for_eyetrackers_task = None
-        self._connected_eye_tracker = None
-        self._handler_tasks = []
-        self._connected_masters = []
-        self._local_addrs = []
-        self._writers = []
-        self._task_list = []
+        self.connected_eye_tracker = None
+        self.masters = []
 
     def _stop_sync(self):
         # sync part of stopping
         for d in self._mounted_drives:
             share.unmount_share(d)
-        for t in self._task_list:
-            t.async_task.cancel()
         if self._ssdp_discovery_task:
             self._ssdp_discovery_task.cancel()
-        for w in self._writers:
-            try:
-                w.close()
-            except:
-                pass
+        with self.master_lock:
+            for m in self.masters:
+                for t in self.masters[m].task_list:
+                    t.async_task.cancel()
 
-    def get_waiters(self):
-        return self._handler_tasks
+                try:
+                    self.masters[m].writer.close()
+                except:
+                    pass
+
+    def get_waiters(self) -> list[asyncio.Task]:
+        with self.master_lock:
+            return [self.masters[m].handler_task for m in self.masters]
 
     async def broadcast(self, type: message.Message, message: str=''):
-        for w in self._writers:
-            await comms.typed_send(w, type, message)
+        with self.master_lock:
+            await asyncio.gather(*[
+                comms.typed_send(self.masters[m].writer, type, message) for m in self.masters
+            ])
 
-    def _remove_finished_task(self, my_task: asyncio.Task):
-        for i,t in enumerate(self._task_list):
+    def _remove_finished_task(self, m: int, my_task: asyncio.Task):
+        for i,t in enumerate(self.masters[m].task_list):
             if t.async_task.get_name()==my_task.get_name():
-                del self._task_list[i]
+                del self.masters[m].task_list[i]
                 break
 
-    async def _handle_master(self, reader: asyncio.streams.StreamReader, writer: asyncio.streams.StreamWriter):
+    async def _handle_master(self, m: int, reader: asyncio.streams.StreamReader, writer: asyncio.streams.StreamWriter):
         type = None
         while type != message.Message.QUIT:
             try:
@@ -184,7 +197,7 @@ class Client:
 
 
                     case message.Message.ET_STATUS_REQUEST:
-                        if not self._connected_eye_tracker:
+                        if not self.connected_eye_tracker:
                             out = eye_tracker.Status.Not_connected
                         else:
                             out = eye_tracker.Status.Connected
@@ -193,10 +206,10 @@ class Client:
                                                {'status': out}
                                               )
                     case message.Message.ET_ATTR_REQUEST:
-                        if not self._connected_eye_tracker:
+                        if not self.connected_eye_tracker:
                             out = None  # none means eye tracker not connected
                         else:
-                            out = eye_tracker.get_attribute_message(self._connected_eye_tracker, msg)
+                            out = eye_tracker.get_attribute_message(self.connected_eye_tracker, msg)
                         await comms.typed_send(writer,
                                                message.Message.ET_ATTR_UPDATE,
                                                out
@@ -211,20 +224,20 @@ class Client:
                             self._mounted_drives.remove(msg['drive'])
 
                     case message.Message.TASK_CREATE:
-                        new_task = task.RunningTask(msg['task_id'])
+                        new_task = task.RunningTask(msg['task_id'], )
                         new_task.async_task = asyncio.create_task(
                             task.Executor().run(
                                 msg['task_id'],msg['type'],msg['payload'],msg['cwd'],msg['env'],msg['interactive'],msg['python_unbuf'],
                                 new_task,
                                 writer)
                         )
-                        self._task_list.append(new_task)
-                        new_task.async_task.add_done_callback(self._remove_finished_task)
+                        self.masters[m].task_list.append(new_task)
+                        new_task.async_task.add_done_callback(lambda tsk: self._remove_finished_task(m, tsk))
 
                     case message.Message.TASK_INPUT:
                         # find if there is a running task with this id and which has an input queue, else ignore the input
                         my_task = None
-                        for t in self._task_list:
+                        for t in self.masters[m].task_list:
                             if msg['task_id']==t.id and not t.async_task.done():
                                 my_task = t
                                 break
@@ -234,7 +247,7 @@ class Client:
                     case message.Message.TASK_CANCEL:
                         # find if there is a running task with this id, else ignore the request
                         my_task = None
-                        for t in self._task_list:
+                        for t in self.masters[m].task_list:
                             if msg['task_id']==t.id and not t.async_task.done():
                                 my_task = t
                                 break
@@ -254,24 +267,20 @@ class Client:
         writer.close()
 
         # remove self from state
-        for i,w in enumerate(self._writers):
-            if w==writer:
-                del self._connected_masters[i]
-                del self._local_addrs[i]
-                del self._writers[i]
-                break
+        with self.master_lock:
+            del self.masters[m]
 
     async def _poll_for_eyetrackers(self):
         try:
             while True:
                 # check if we have an eye tracker
                 et = eye_tracker.get()
-                if not self._connected_eye_tracker and et:
-                    self._connected_eye_tracker = et
-                    eye_tracker.subscribe_to_notifications(self._connected_eye_tracker, self.broadcast)
+                if not self.connected_eye_tracker and et:
+                    self.connected_eye_tracker = et
+                    eye_tracker.subscribe_to_notifications(self.connected_eye_tracker, self.broadcast)
                     await self.broadcast(message.Message.ET_STATUS_INFORM, {'status': eye_tracker.Status.Connected})
-                elif not et and self._connected_eye_tracker:
-                    self._connected_eye_tracker = None
+                elif not et and self.connected_eye_tracker:
+                    self.connected_eye_tracker = None
                     await self.broadcast(message.Message.ET_STATUS_INFORM, {'status': eye_tracker.Status.Not_connected})
 
                 # rate-limit to every x seconds
