8da859456300d2643aa507e2f73b647c4e058331
Diederick Niehorster <dcnieho@gmail.com>
2023-12-23 16:05:48 +0100

    client: make it possible to log to console (default is log to file)

--- example_scripts/client.py
+++ example_scripts/client.py
@@ -6,17 +6,20 @@ import ctypes
 import labManager.client
 import labManager.common
 
-# set up some simple logging
-import sys
-import tempfile
-temp_dir = pathlib.Path(tempfile.gettempdir())
-sys.stdout = open(temp_dir / "labManager_client_stdout.txt", "w")
-sys.stderr = open(temp_dir / "labManager_client_stderr.txt", "w")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="labManager client")
     parser.add_argument('--hide', action='store_true', help="hide console window")
+    parser.add_argument('--log_to_console', action='store_true', help="log to console")
     args = parser.parse_args()
+
+    if not args.log_to_console:
+        # set up some simple logging to file
+        import sys
+        import tempfile
+        temp_dir = pathlib.Path(tempfile.gettempdir())
+        sys.stdout = open(temp_dir / "labManager_client_stdout.txt", "w")
+        sys.stderr = open(temp_dir / "labManager_client_stderr.txt", "w")
 
     if args.hide:
         ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
