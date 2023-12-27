cb1cc242d59ee86e482f32eb095b80fe3b4c9652
Diederick Niehorster <dcnieho@gmail.com>
2023-12-27 20:12:48 +0100

    filepicker: don't shadow id builtin

--- labManager-master/src/labManager/GUI/master/_impl/filepicker.py
+++ labManager-master/src/labManager/GUI/master/_impl/filepicker.py
@@ -93,10 +93,10 @@ class FilePicker:
             # update selected
             got_one = False
             for p in paths:
-                for id in self.items:
-                    entry = self.items[id]
-                    if entry.full_path==p and (not self.predicate or self.predicate(id)):
-                        self.selected[id] = True
+                for iid in self.items:
+                    entry = self.items[iid]
+                    if entry.full_path==p and (not self.predicate or self.predicate(iid)):
+                        self.selected[iid] = True
                         got_one = True
                         break
                 if not self.allow_multiple and got_one:
@@ -142,7 +142,7 @@ class FilePicker:
 
     def _refresh_path_done(self, items: list[structs.DirEntry]|Exception):
         if not self.new_loc:
-            selected = [self.items[id] for id in self.items if id in self.selected and self.selected[id]]
+            selected = [self.items[iid] for iid in self.items if iid in self.selected and self.selected[iid]]
         self.items.clear()
         self.selected.clear()
         self.msg = None
@@ -157,10 +157,10 @@ class FilePicker:
         # if refreshed the same directory, restore old selection
         if not self.new_loc:
             for old in selected:
-                for id in self.items:
-                    entry = self.items[id]
+                for iid in self.items:
+                    entry = self.items[iid]
                     if entry.name==old.name:
-                        self.selected[id] = True
+                        self.selected[iid] = True
                         break
 
         self.require_sort = True
@@ -258,12 +258,12 @@ class FilePicker:
                     # Headers
                     imgui.table_next_row(imgui.TableRowFlags_.headers)
                     # checkbox column: reflects whether all, some or none of visible recordings are selected, and allows selecting all or none
-                    num_selected = sum([self.selected[id] for id in self.selected])
+                    num_selected = sum([self.selected[iid] for iid in self.selected])
                     if self.allow_multiple:
                         imgui.table_set_column_index(0)
                         # determine state
                         if self.predicate:
-                            num_items = sum([self.predicate(id) for id in self.items])
+                            num_items = sum([self.predicate(iid) for iid in self.items])
                         else:
                             num_items = len(self.items)
                         if num_selected==0:
@@ -295,14 +295,14 @@ class FilePicker:
                     if self.sorted_items and self.last_clicked_id not in self.sorted_items:
                         # default to topmost if last_clicked unknown, or no longer on screen due to filter
                         self.last_clicked_id = self.sorted_items[0]
-                    for id in self.sorted_items:
+                    for iid in self.sorted_items:
                         imgui.table_next_row()
 
                         selectable_clicked = False
                         checkbox_clicked, checkbox_hovered, checkbox_out = False, False, False
                         has_drawn_hitbox = False
 
-                        disable_item = self.predicate and not self.predicate(id)
+                        disable_item = self.predicate and not self.predicate(iid)
                         if disable_item:
                             imgui.internal.push_item_flag(imgui.internal.ItemFlags_.disabled, True)
                             imgui.push_style_var(imgui.StyleVar_.alpha, imgui.get_style().alpha * 0.5)
@@ -324,7 +324,7 @@ class FilePicker:
                                 imgui.push_style_var(imgui.StyleVar_.frame_border_size, 0.)
                                 imgui.push_style_var(imgui.StyleVar_.frame_padding    , (0.,0.))
                                 imgui.push_style_var(imgui.StyleVar_.item_spacing     , (0.,cell_padding_y))
-                                selectable_clicked, selectable_out = imgui.selectable(f"##{id}_hitbox", self.selected[id], flags=imgui.SelectableFlags_.span_all_columns|imgui.SelectableFlags_.allow_overlap|imgui.internal.SelectableFlagsPrivate_.select_on_click, size=(0,frame_height+cell_padding_y))
+                                selectable_clicked, selectable_out = imgui.selectable(f"##{iid}_hitbox", self.selected[iid], flags=imgui.SelectableFlags_.span_all_columns|imgui.SelectableFlags_.allow_overlap|imgui.internal.SelectableFlagsPrivate_.select_on_click, size=(0,frame_height+cell_padding_y))
                                 imgui.set_cursor_pos_y(cur_pos_y)   # instead of imgui.same_line(), we just need this part of its effect
                                 imgui.pop_style_var(3)
                                 has_drawn_hitbox = True
@@ -335,7 +335,7 @@ class FilePicker:
                                 imgui.push_style_var(imgui.StyleVar_.frame_padding    , (0.,imgui.get_style().frame_padding.y))
                                 imgui.push_style_var(imgui.StyleVar_.item_spacing     , (0.,imgui.get_style().item_spacing.y))
                                 imgui.push_style_color(imgui.Col_.button, (0.,0.,0.,0.))
-                                imgui.button(f"##{id}_id", size=(imgui.FLT_MIN,0))
+                                imgui.button(f"##{iid}_id", size=(imgui.FLT_MIN,0))
                                 imgui.pop_style_color()
                                 imgui.pop_style_var(3)
 
@@ -344,25 +344,25 @@ class FilePicker:
                             match ci+int(not self.allow_multiple):
                                 case 0:
                                     # Selector
-                                    checkbox_clicked, checkbox_out = utils.my_checkbox(f"##{id}_selected", self.selected[id], frame_size=(0,0))
+                                    checkbox_clicked, checkbox_out = utils.my_checkbox(f"##{iid}_selected", self.selected[iid], frame_size=(0,0))
                                     checkbox_hovered = imgui.is_item_hovered()
                                 case 1:
                                     # Name
-                                    imgui.text(self.items[id].display_name)
+                                    imgui.text(self.items[iid].display_name)
                                 case 2:
                                     # Date created
-                                    imgui.text(self.items[id].ctime_str)
+                                    imgui.text(self.items[iid].ctime_str)
                                 case 3:
                                     # Date modified
-                                    imgui.text(self.items[id].mtime_str)
+                                    imgui.text(self.items[iid].mtime_str)
                                 case 4:
                                     # Type
-                                    if self.items[id].mime_type:
-                                        imgui.text(self.items[id].mime_type)
+                                    if self.items[iid].mime_type:
+                                        imgui.text(self.items[iid].mime_type)
                                 case 5:
                                     # Size
-                                    if self.items[id].size_str:
-                                        imgui.text(self.items[id].size_str)
+                                    if self.items[iid].size_str:
+                                        imgui.text(self.items[iid].size_str)
 
                         if disable_item:
                             imgui.internal.pop_item_flag()
@@ -372,7 +372,7 @@ class FilePicker:
                         # NB: any_selectable_clicked is just for handling clicks not on any item
                         any_selectable_clicked = any_selectable_clicked or selectable_clicked
                         self.last_clicked_id = utils.selectable_item_logic(
-                            id, self.selected, self.last_clicked_id, self.sorted_items,
+                            iid, self.selected, self.last_clicked_id, self.sorted_items,
                             selectable_clicked, selectable_out, allow_multiple=self.allow_multiple,
                             overlayed_hovered=checkbox_hovered, overlayed_clicked=checkbox_clicked, new_overlayed_state=checkbox_out
                             )
@@ -380,12 +380,12 @@ class FilePicker:
                         # further deal with doubleclick on item
                         if selectable_clicked and not checkbox_hovered: # don't enter this branch if interaction is with checkbox on the table row
                             if not imgui.get_io().key_ctrl and not imgui.get_io().key_shift and imgui.is_mouse_double_clicked(imgui.MouseButton_.left):
-                                if self.items[id].is_dir:
-                                    self.goto(self.items[id].full_path)
+                                if self.items[iid].is_dir:
+                                    self.goto(self.items[iid].full_path)
                                     break
                                 else:
                                     utils.set_all(self.selected, False)
-                                    self.selected[id] = True
+                                    self.selected[iid] = True
                                     imgui.close_current_popup()
                                     closed = True
 
@@ -407,7 +407,7 @@ class FilePicker:
                 cancelled = closed = True
             # Ok button
             imgui.same_line()
-            num_selected = sum([self.selected[id] for id in self.selected])
+            num_selected = sum([self.selected[iid] for iid in self.selected])
             disable_ok = not num_selected
             if disable_ok:
                 imgui.internal.push_item_flag(imgui.internal.ItemFlags_.disabled, True)
@@ -426,7 +426,7 @@ class FilePicker:
             cancelled = closed = True
         if closed:
             if not cancelled and self.callback:
-                selected = [self.items[id].full_path for id in self.items if id in self.selected and self.selected[id]]
+                selected = [self.items[iid].full_path for iid in self.items if iid in self.selected and self.selected[iid]]
                 self.callback(selected if selected else None)
         return opened, closed
 
@@ -437,21 +437,21 @@ class FilePicker:
             for sort_spec in reversed(sort_specs):
                 match sort_spec.column_index+int(not self.allow_multiple):
                     case 2:     # Date created
-                        key = lambda id: self.items[id].ctime
+                        key = lambda iid: self.items[iid].ctime
                     case 3:     # Date modified
-                        key = lambda id: self.items[id].mtime
+                        key = lambda iid: self.items[iid].mtime
                     case 4:     # Type
-                        key = lambda id: m if (m:=self.items[id].mime_type) else ''
+                        key = lambda iid: m if (m:=self.items[iid].mime_type) else ''
                     case 5:     # Size
-                        key = lambda id: self.items[id].size
+                        key = lambda iid: self.items[iid].size
 
                     case _:     # Name and all others
-                        key = natsort.os_sort_keygen(key=lambda id: self.items[id].full_path)
+                        key = natsort.os_sort_keygen(key=lambda iid: self.items[iid].full_path)
 
                 ids.sort(key=key, reverse=bool(sort_spec.get_sort_direction() - 1))
 
             # finally, always sort dirs first
-            ids.sort(key=lambda id: self.items[id].is_dir, reverse=True)
+            ids.sort(key=lambda iid: self.items[iid].is_dir, reverse=True)
             self.sorted_items = ids
             sort_specs_in.specs_dirty = False
             self.require_sort = False
