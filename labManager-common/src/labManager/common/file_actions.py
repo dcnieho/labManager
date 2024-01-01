97a0edac6dc8edfb8122f980dc232a78d06c3239
Diederick Niehorster <dcnieho@gmail.com>
2024-01-01 15:22:05 +0100

    file_actions: get_dir_list: be robust to files disappearing while the directory is being listed

--- labManager-common/src/labManager/common/file_actions.py
+++ labManager-common/src/labManager/common/file_actions.py
@@ -63,10 +63,15 @@ async def get_dir_list(path: pathlib.Path) -> list[structs.DirEntry]:
     path = aiopath.AsyncPath(path)
     out = []
     async for e in path.iterdir():
-        stat, is_dir = await asyncio.gather(e.stat(), e.is_dir())
-        out.append(structs.DirEntry(e.name, is_dir, pathlib.Path(e),
+        try:
+            stat, is_dir = await asyncio.gather(e.stat(), e.is_dir())
+            item = structs.DirEntry(e.name, is_dir, pathlib.Path(e),
                                     stat.st_ctime, stat.st_mtime, stat.st_size,
-                                    mimetypes.guess_type(e)[0]))
+                                    mimetypes.guess_type(e)[0])
+        except:
+            pass
+        else:
+            out.append(item)
 
     return out
 
