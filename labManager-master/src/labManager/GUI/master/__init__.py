e550b62e3fb4da3c88962f055aeccd80d880be1a
Diederick Niehorster <dcnieho@gmail.com>
2024-01-02 12:54:29 +0100

    GUI: provide file_picker with network config

--- labManager-master/src/labManager/GUI/master/__init__.py
+++ labManager-master/src/labManager/GUI/master/__init__.py
@@ -763,7 +763,7 @@ class MainGUI:
                     if imgui.button("Append path"):
                         def append_path(tsk: TaskDef, path: str):
                             tsk.payload_text += str(path[0])
-                        file_action_provider_args = {'network': None, 'master': self.master}
+                        file_action_provider_args = {'network': config.master['network'], 'master': self.master}
                         utils.push_popup(self, filepicker.FilePicker(title='Select path to append', allow_multiple=False, file_action_provider_args=file_action_provider_args, callback=lambda path: append_path(self._task_prep, path)))
                 else:
                     imgui.push_font(imgui_md.get_code_font())
