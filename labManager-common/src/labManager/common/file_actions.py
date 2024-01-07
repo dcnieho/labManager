e4e4dbc734cd5a319f1de29e73e209ae82e1ac0d
Diederick Niehorster <dcnieho@gmail.com>
2024-01-08 00:26:58 +0100

    file_actions: added function for listing shares using network enumerator

--- labManager-common/src/labManager/common/file_actions.py
+++ labManager-common/src/labManager/common/file_actions.py
@@ -12,9 +12,50 @@ import ctypes
 _kernel32 = ctypes.WinDLL('kernel32',use_last_error=True)
 _mpr = ctypes.WinDLL('mpr',use_last_error=True)
 
+class NETRESOURCE(ctypes.Structure):
+	_fields_ = [
+		('scope', ctypes.wintypes.DWORD),
+		('type', ctypes.wintypes.DWORD),
+		('display_type', ctypes.wintypes.DWORD),
+		('usage', ctypes.wintypes.DWORD),
+		('local_name', ctypes.wintypes.LPWSTR),
+		('remote_name', ctypes.wintypes.LPWSTR),
+		('comment', ctypes.wintypes.LPWSTR),
+		('provider', ctypes.wintypes.LPWSTR),
+	]
+LPNETRESOURCE = ctypes.POINTER(NETRESOURCE)
+
+RESOURCE_CONNECTED  = 0x00000001
+RESOURCE_GLOBALNET  = 0x00000002
+RESOURCE_REMEMBERED = 0x00000003
+RESOURCE_RECENT     = 0x00000004
+RESOURCE_CONTEXT    = 0x00000005
+RESOURCETYPE_ANY        = 0x00000000
+RESOURCETYPE_DISK       = 0x00000001
+RESOURCETYPE_PRINT      = 0x00000002
+RESOURCETYPE_RESERVED   = 0x00000008
+RESOURCETYPE_UNKNOWN    = 0xFFFFFFFF
+RESOURCEUSAGE_CONNECTABLE   = 0x00000001
+RESOURCEUSAGE_CONTAINER     = 0x00000002
+RESOURCEUSAGE_NOLOCALDEVICE = 0x00000004
+RESOURCEUSAGE_SIBLING       = 0x00000008
+RESOURCEUSAGE_ATTACHED      = 0x00000010
+RESOURCEDISPLAYTYPE_GENERIC         = 0x00000000
+RESOURCEDISPLAYTYPE_DOMAIN          = 0x00000001
+RESOURCEDISPLAYTYPE_SERVER          = 0x00000002
+RESOURCEDISPLAYTYPE_SHARE           = 0x00000003
+RESOURCEDISPLAYTYPE_FILE            = 0x00000004
+RESOURCEDISPLAYTYPE_GROUP           = 0x00000005
+RESOURCEDISPLAYTYPE_NETWORK         = 0x00000006
+RESOURCEDISPLAYTYPE_ROOT            = 0x00000007
+RESOURCEDISPLAYTYPE_SHAREADMIN      = 0x00000008
+RESOURCEDISPLAYTYPE_DIRECTORY       = 0x00000009
+RESOURCEDISPLAYTYPE_TREE            = 0x0000000A
+RESOURCEDISPLAYTYPE_NDSCONTAINER    = 0x0000000B
 
 ERROR_NO_MORE_ITEMS = 259
 ERROR_EXTENDED_ERROR= 1208
+
 def _error_check_non0_is_error(result, func, args):
     if not result or result==ERROR_NO_MORE_ITEMS:
         return result
@@ -24,15 +65,46 @@ def _error_check_non0_is_error(result, func, args):
         description = ctypes.create_unicode_buffer(256)
         WNetGetLastError(ctypes.byref(last_error),description,ctypes.sizeof(description),provider,ctypes.sizeof(provider))
         raise ctypes.WinError(last_error.value, f'{provider.value} failed: {description.value}')
-    raise ctypes.WinError(ctypes.get_last_error())
+    if (err := ctypes.get_last_error()):
+        result = err
+    raise ctypes.WinError(result)
 def _error_check_0_is_error(result, func, args):
     if not result:
-         raise ctypes.WinError(ctypes.get_last_error())
+        raise ctypes.WinError(ctypes.get_last_error())
 
 WNetGetLastError = _mpr.WNetGetLastErrorW
 WNetGetLastError.argtypes = ctypes.wintypes.LPDWORD, ctypes.wintypes.LPWSTR, ctypes.wintypes.DWORD, ctypes.wintypes.LPWSTR, ctypes.wintypes.DWORD
 WNetGetLastError.restype = ctypes.wintypes.DWORD
 WNetGetLastError.errcheck = _error_check_non0_is_error
+
+WNetOpenEnum = _mpr.WNetOpenEnumW
+WNetOpenEnum.argtypes = ctypes.wintypes.DWORD, ctypes.wintypes.DWORD, ctypes.wintypes.DWORD, LPNETRESOURCE, ctypes.wintypes.LPHANDLE
+WNetOpenEnum.restype = ctypes.wintypes.DWORD
+WNetOpenEnum.errcheck = _error_check_non0_is_error
+
+WNetEnumResource = _mpr.WNetEnumResourceW
+WNetEnumResource.argtypes = ctypes.wintypes.HANDLE, ctypes.wintypes.LPDWORD, ctypes.wintypes.LPVOID, ctypes.wintypes.LPDWORD
+WNetEnumResource.restype = ctypes.wintypes.DWORD
+WNetEnumResource.errcheck = _error_check_non0_is_error
+
+WNetCloseEnum = _mpr.WNetCloseEnum
+WNetCloseEnum.argtypes = ctypes.wintypes.HANDLE,
+WNetCloseEnum.restype = ctypes.wintypes.DWORD
+WNetCloseEnum.errcheck = _error_check_non0_is_error
+
+
+GMEM_FIXED    = 0x0000
+GMEM_MOVEABLE = 0x0002
+GMEM_ZEROINIT = 0x0040
+GHND          = GMEM_ZEROINIT | GMEM_MOVEABLE
+GPTR          = GMEM_ZEROINIT | GMEM_FIXED
+GlobalAlloc = _kernel32.GlobalAlloc
+GlobalAlloc.argtypes = ctypes.wintypes.UINT, ctypes.c_ssize_t
+GlobalAlloc.restype = ctypes.wintypes.HANDLE
+GlobalFree = _kernel32.GlobalFree
+GlobalFree.argtypes = ctypes.wintypes.HANDLE,
+GlobalFree.restype = ctypes.wintypes.HANDLE
+GlobalFree.errcheck = _error_check_non0_is_error
 
 class UNIVERSAL_NAME_INFO(ctypes.Structure):
 	_fields_ = [
@@ -143,6 +215,46 @@ def get_drives() -> list[structs.DirEntry]:
         bitmask >>= 1
 
     return drives
+
+def get_shares(server: str):
+    # TODO: use WNetAddConnection2W to support credentials. CONNECT_TEMPORARY, no redirect and also close connection after
+    # should support same interface as smb.py functions. probably should move the matching etc logic
+    # to the master or some generic helper
+    shares: list[structs.DirEntry] = []
+
+    # user should have provided credentials for this server (i.e.
+    # if you can open it in explorer, you can list it using this call)
+    server = server.strip('\\/')
+    nr = NETRESOURCE(scope=RESOURCE_GLOBALNET, type=RESOURCETYPE_ANY, remote_name=f'\\\\{server}')
+
+    hEnum = ctypes.wintypes.HANDLE()
+    try:
+        WNetOpenEnum(RESOURCE_GLOBALNET, RESOURCETYPE_ANY, 0, nr, ctypes.byref(hEnum))
+    except Exception as exc:
+        print(exc)
+        return
+
+    # allocate a buffer
+    cbBuffer = ctypes.wintypes.DWORD(16384)     # 16K is a good size
+    mem_handle = GlobalAlloc(GPTR, cbBuffer.value)
+    lpnrLocal = ctypes.cast(mem_handle,LPNETRESOURCE)
+
+    while True:
+        cEntries = ctypes.wintypes.DWORD(-1)        # enumerate all possible entries
+        result = WNetEnumResource(hEnum, ctypes.byref(cEntries),lpnrLocal,ctypes.byref(cbBuffer))
+        if result==ERROR_NO_MORE_ITEMS:
+            break
+
+        for idx in range(cEntries.value):
+            share = lpnrLocal[idx]
+            share_path = share.remote_name
+            path_comps = get_net_computer(share_path)
+            if len(path_comps)>=2:
+                shares.append(structs.DirEntry(path_comps[1],True,pathlib.Path(share_path),None,None,None,'labManager/net_share'))
+
+    # clean up
+    GlobalFree(mem_handle)
+    WNetCloseEnum(hEnum)
 
 async def get_dir_list(path: pathlib.Path) -> list[structs.DirEntry]:
     # will throw when path doesn't exist or is not a directory
