976d7d842ee58c0859bfe0e5feb9d665cfab3aa8
Diederick Niehorster <dcnieho@gmail.com>
2024-01-18 12:12:41 +0100

    file_commander: keep done button visible when no clients selected

--- labManager-master/src/labManager/GUI/master/_impl/file_commander.py
+++ labManager-master/src/labManager/GUI/master/_impl/file_commander.py
@@ -68,56 +68,57 @@ class FileCommander:
         with self.master.clients_lock:
             selected_clients = [c for c in self.selected_clients if self.selected_clients[c] and c in self.master.clients and self.master.clients[c].online]
             computers_txt = ', '.join((self.master.clients[i].name for i in selected_clients))
-        if not selected_clients:
-            imgui.text_wrapped('You do not have any running clients selected in the main GUI, nothing to do here')
-            imgui.end_child()
-            return
 
-        imgui.text_wrapped('The action you do in this interface will be performed on the following computers: '+computers_txt)
-
-        # figure out layout: get width of middle section, divide leftover equally between the other two
-        # widest element is checkbox, get its width
-        cb_label = 'Append station folder?'
-        w = imgui.get_frame_height() + imgui.get_style().item_inner_spacing.x + imgui.calc_text_size(cb_label).x + 2*imgui.get_style().frame_padding.x
-
+        # figure out layout
         space = imgui.get_content_region_avail()
         button_text_size = imgui.calc_text_size(icons_fontawesome.ICON_FA_BAN+" Cancel")
         bottom_margin = button_text_size.y+imgui.get_style().frame_padding.y*2+imgui.get_style().item_spacing.y
         space.y = -bottom_margin
+        if not selected_clients:
+            imgui.begin_child('##nothing-to-do',size=space)
+            imgui.text_wrapped('You do not have any running clients selected in the main GUI, nothing to do here')
+            imgui.end_child()
+        else:
+            imgui.text_wrapped('The action you do in this interface will be performed on the following computers: '+computers_txt)
 
-        imgui.begin_child('##left_picker',size=((space.x-w)/2,space.y))
-        self.left.draw_top_bar()
-        self.left.draw_listing(leave_space_for_bottom_bar=False)
-        imgui.end_child()
-        imgui.same_line()
+            # figure out layout: get width of middle section, divide leftover equally between the other two
+            # widest element is checkbox, get its width
+            cb_label = 'Append station folder?'
+            w = imgui.get_frame_height() + imgui.get_style().item_inner_spacing.x + imgui.calc_text_size(cb_label).x + 2*imgui.get_style().frame_padding.x
 
-        imgui.begin_child('##actions',size=(w,space.y))
-        enabled = self.left.machine.startswith(filepicker.FileActionProvider.remote_prefix) and self.right.machine.startswith(filepicker.FileActionProvider.remote_prefix)
-        enabled = enabled and any(self.left.selected.values())
-        if not enabled:
-            utils.push_disabled()
-        imgui.push_font(self.mainGUI.icon_font)
-        # get button size, center horizontally and vertically
-        button_text_size = imgui.calc_text_size(icons_fontawesome.ICON_FA_ARROW_RIGHT)
-        button_size = [t+p*2 for t,p in zip(button_text_size,imgui.get_style().frame_padding)]#+imgui.get_style().item_spacing.y
-        margin = [(a-b)/2 for a,b in zip(imgui.get_content_region_avail(), button_size)]
-        imgui.set_cursor_pos([a+b for a,b in zip(imgui.get_cursor_pos(),margin)])
-        if imgui.button(icons_fontawesome.ICON_FA_ARROW_RIGHT+"##action"):
-            self.do_copy()
-        imgui.pop_font()
-        # center checkbox+label horizontally
-        imgui.set_cursor_pos_x(imgui.get_cursor_pos_x()+(imgui.get_content_region_avail().x-w)/2)
-        self.append_computer_name = imgui.checkbox(cb_label,self.append_computer_name)[1]
-        if not enabled:
-            utils.pop_disabled()
-        utils.draw_hover_text('When enabled, a folder will be made for each machine to receive the files copied from the left side. That means you won\'t have all files mixed together, but organized in folders per machine.', text='', hovered_flags=imgui.HoveredFlags_.allow_when_disabled)
-        imgui.end_child()
-        imgui.same_line()
+            imgui.begin_child('##left_picker',size=((space.x-w)/2,space.y))
+            self.left.draw_top_bar()
+            self.left.draw_listing(leave_space_for_bottom_bar=False)
+            imgui.end_child()
+            imgui.same_line()
 
-        imgui.begin_child('##right_picker',size=((space.x-w)/2,space.y))
-        self.right.draw_top_bar()
-        self.right.draw_listing(leave_space_for_bottom_bar=False)
-        imgui.end_child()
+            imgui.begin_child('##actions',size=(w,space.y))
+            enabled = self.left.machine.startswith(filepicker.FileActionProvider.remote_prefix) and self.right.machine.startswith(filepicker.FileActionProvider.remote_prefix)
+            enabled = enabled and any(self.left.selected.values())
+            if not enabled:
+                utils.push_disabled()
+            imgui.push_font(self.mainGUI.icon_font)
+            # get button size, center horizontally and vertically
+            button_text_size = imgui.calc_text_size(icons_fontawesome.ICON_FA_ARROW_RIGHT)
+            button_size = [t+p*2 for t,p in zip(button_text_size,imgui.get_style().frame_padding)]#+imgui.get_style().item_spacing.y
+            margin = [(a-b)/2 for a,b in zip(imgui.get_content_region_avail(), button_size)]
+            imgui.set_cursor_pos([a+b for a,b in zip(imgui.get_cursor_pos(),margin)])
+            if imgui.button(icons_fontawesome.ICON_FA_ARROW_RIGHT+"##action"):
+                self.do_copy()
+            imgui.pop_font()
+            # center checkbox+label horizontally
+            imgui.set_cursor_pos_x(imgui.get_cursor_pos_x()+(imgui.get_content_region_avail().x-w)/2)
+            self.append_computer_name = imgui.checkbox(cb_label,self.append_computer_name)[1]
+            if not enabled:
+                utils.pop_disabled()
+            utils.draw_hover_text('When enabled, a folder will be made for each machine to receive the files copied from the left side. That means you won\'t have all files mixed together, but organized in folders per machine.', text='', hovered_flags=imgui.HoveredFlags_.allow_when_disabled)
+            imgui.end_child()
+            imgui.same_line()
+
+            imgui.begin_child('##right_picker',size=((space.x-w)/2,space.y))
+            self.right.draw_top_bar()
+            self.right.draw_listing(leave_space_for_bottom_bar=False)
+            imgui.end_child()
 
         closed = False
         if imgui.button(icons_fontawesome.ICON_FA_CHECK+" Done"):
